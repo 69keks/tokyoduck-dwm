3df5abb0eac83dd28cb377733b1656d9a1aa363a
Leviathan melvilli <mehere.youthere@pm.me>
2024-11-06 14:26:05 +0530

    Added XF86 configuration capabilities
    This change includes the <X11/XF86keysym.h> file which enables us to add
    control of keyboard, display brightnesses and volume controls through
    the on-keyboard XF86 extra keys.

--- config.def.h
+++ config.def.h
@@ -1,5 +1,8 @@
 /* See LICENSE file for copyright and license details. */
 
+/* Adding XF86 functionality to DWM */
+#include <X11/XF86keysym.h>
+
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
@@ -69,6 +72,15 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", spaceduck, NULL };
 static const char *termcmd[]  = { "st", NULL };
 
+/* brightness */
+static const char *brightness_down[]    = { "brillo", "-u", "150000", "-U", "5", NULL };
+static const char *brightness_up[]      = { "brillo", "-u", "150000", "-A", "5", NULL };
+
+/* Volume Control */
+static const char *volup[]      = { "pactl", "set-sink-volume", "@DEFAULT_SINK@", "+5%", NULL };
+static const char *voldown[]    = { "pactl", "set-sink-volume", "@DEFAULT_SINK@", "-5%", NULL };
+static const char *mutevol[]    = { "pactl", "set-sink-mute", "@DEFAULT_SINK@", "toggle", NULL };
+
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
@@ -94,6 +106,13 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
+  { 0,                            XF86XK_MonBrightnessUp, spawn,  {.v = brightness_up } },
+	{ 0,                            XF86XK_MonBrightnessDown,       spawn,  {.v = brightness_down } },
+	/*{ 0,                            XF86XK_KbdBrightnessUp, spawn,  {.v = kbdbrightness_up } },*/
+	/*{ 0,                            XF86XK_KbdBrightnessDown,       spawn, {.v = kbdbrightness_down } },*/
+	{ 0,                            XF86XK_AudioRaiseVolume,        spawn, {.v = volup } },
+	{ 0,                            XF86XK_AudioLowerVolume,        spawn, {.v = voldown } },
+	{ 0,                            XF86XK_AudioMute,               spawn, {.v = mutevol } },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
